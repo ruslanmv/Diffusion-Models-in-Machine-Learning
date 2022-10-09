64efed6065693259003ccba16dbc1132507ddc00
Ruslan Magana Vsevolodovna <51296249+ruslanmv@users.noreply.github.com>
2022-10-09 13:51:34 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #SageMaker Setup
 source activate python3
-cd Sagemaker
+cd SageMaker
 git clone https://github.com/ruslanmv/Diffusion-Models-in-Machine-Learning.git
 cd Diffusion-Models-in-Machine-Learning/
-conda create -n difussion python=3.8
-conda activate difussion
+conda create -n diffusion python=3.8
+conda activate diffusion
 pip install  pytorch pytorch_lightning  imageio torchvision
+conda install ipykernel -y
+python -m ipykernel install --user --name difussion --display-name "Python (difussion)"
